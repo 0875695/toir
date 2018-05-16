7d31ca8a535b4e1d37e77edbc8703ddbe3a91206
Aleksey Ivanov <0875695@gmail.com>
2018-05-16 09:19:00 +0300

    Create user function

--- imports/api/users/server/methods.js
+++ imports/api/users/server/methods.js
@@ -1,34 +1,32 @@
-import {
-   usersAddFormSchema
-} from '../users-add-form-schema.js';
+import {usersAddFormSchema} from '../users-add-form-schema.js';
 
 Meteor.methods({
    'users.add' (data) {
-      console.log(data);
-
       usersAddFormSchema.validate(data);
-      //throw new Meteor.Error(403, "Access denied");
 
-   //    if (Meteor.users.findOne({
-   //          'emails.address': user.email
-   //       })) {
-   //       return;
-   //    }
-   //
-   //    var id;
-   //
-   //    id = Accounts.createUser({
-   //       email: user.email,
-   //       password: 'Init123$',
-   //       profile: {
-   //          name: user.name
-   //       }
-   //    });
-   //
-   //    if (user.roles.length > 0) {
-   //       // Need _id of existing user record so this call must come
-   //       // after `Accounts.createUser` or `Accounts.onCreate`
-   //       Roles.addUsersToRoles(id, user.roles, 'admin-group');
-   //    }
+      let loggedInUser = Meteor.user();
+      if (!loggedInUser ||
+         !Roles.userIsInRole(loggedInUser, ['admin'], 'toir-group')) {
+         throw new Meteor.Error(403, 'Access denied');
+      }
+      if (Meteor.users.findOne({
+            'emails.address': data.email
+         })) {
+         throw new Meteor.Error(500, 'That email address already registered');
+      }
+
+      var id;
+
+      id = Accounts.createUser({
+         email: data.email,
+         password: data.password,
+         profile: {
+            name: data.name
+         }
+      });
+
+      if (data.roles.length > 0) {
+         Roles.addUsersToRoles(id, data.roles, 'toir-group');
+      }
    }
 });
