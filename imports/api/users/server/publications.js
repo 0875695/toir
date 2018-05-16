086279f52a27510d39fa18c67686fa42f4bf94af
Aleksey Ivanov <0875695@gmail.com>
2018-05-16 12:08:11 +0300

    usersUpdateFormSchema

---
+++ imports/api/users/server/publications.js
@@ -0,0 +1,15 @@
+Meteor.publish('users.all', function () {
+  let loggedInUser = Meteor.users.findOne(this.userId);
+  if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['admin'], 'toir')) {
+     return [];
+  }
+  return Meteor.users.find();
+});
+Meteor.publish('users.one', function (uId) {
+  let loggedInUser = Meteor.users.findOne(this.userId);
+  if (!loggedInUser || !Roles.userIsInRole(loggedInUser, ['admin'], 'toir')) {
+
+    return [];
+  }
+  return Meteor.users.find({_id: uId});
+});
